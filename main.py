8ffe8ad9f9f8062e1f54cdef0d3847b9f9ea64eb
Arzamastsev Dmitry <motok1996@gmail.com>
2021-09-15 04:16:46 +0300

    v 0.2 Classes

--- main.py
+++ main.py
@@ -1,36 +1,6 @@
-import os
-import time
-import requests
-from tok import token
-URL = 'https://api.vk.com/method/photos.get'
-params = {
-    'user_id': '87878521',
-    'album_id': 'profile',
-    'extended': '1',
-    'photo_sizes': '1',
-    'access_token': token,
-    'count': '1000',
-    'v': '5.131'
-}
-res = requests.get(URL, params=params)
-for item in res.json()['response']['items']:
-    likes_count = item["likes"]["count"]
-    if not os.path.exists(f'Photos/{likes_count}.jpg'):
-        with open(f'Photos/{likes_count}.jpg', 'wb') as file:
-            photo = requests.get(item['sizes'][-1]['url'])
-            file.write(photo.content)
-    else:
-        with open(f'Photos/{likes_count}_{item["date"]}.jpg', 'wb') as file:
-            photo = requests.get(item['sizes'][-1]['url'])
-            file.write(photo.content)
-    time.sleep(0.5)
+from tok import tok
+from vk import Vk
 
 
-
-
-
-
-
-# pprint(res.json())
-# with open('1.json', 'w', encoding='utf-8') as file:
-#     json.dump(res.json(), file, indent=4)
+my_vk = Vk(tok, 87878521)
+my_vk.choice_album()
---
+++ vk.py
@@ -0,0 +1,87 @@
+import os
+from time import sleep
+import requests
+from tqdm import tqdm
+
+
+def download_photos(photo_list):
+    for item in tqdm(photo_list):
+        likes_count = item["likes"]["count"]
+        if not os.path.exists(f'Photos/{likes_count}.jpg'):
+            with open(f'Photos/{likes_count}.jpg', 'wb') as file:
+                photo = requests.get(item['sizes'][-1]['url'])
+                file.write(photo.content)
+        else:
+            with open(f'Photos/{likes_count}_{item["date"]}.jpg', 'wb') as file:
+                photo = requests.get(item['sizes'][-1]['url'])
+                file.write(photo.content)
+        sleep(0.5)
+
+
+class Vk:
+    def __init__(self, token, owner_id):
+        self.token = token
+        self.owner_id = owner_id
+        self.url = 'https://api.vk.com/method/'
+
+    def _get_albums(self):
+        params = {
+            'owner_id': self.owner_id,
+            'photo_sizes': '1',
+            'need_system': '1',
+            'access_token': self.token,
+            'v': '5.131'
+        }
+
+        res = requests.get(self.url+'photos.getAlbums', params=params)
+        return res.json()['response']['items']
+
+    def _get_photos_list(self, album_id):
+        params = {
+            'owner_id': self.owner_id,
+            'album_id': album_id,
+            'extended': '1',
+            'count': '1000',
+            'photo_sizes': '1',
+            'access_token': self.token,
+            'v': '5.131'
+        }
+        res = requests.get(self.url+'photos.get', params=params)
+        download_photos(res.json()['response']['items'])
+
+    def _get_user_photos_list(self):
+        params = {
+            'user_id': self.owner_id,
+            'extended': '1',
+            'count': '1000',
+            'access_token': self.token,
+            'v': '5.131'
+        }
+
+        res = requests.get(self.url + 'photos.getUserPhotos', params=params)
+        download_photos(res.json()['response']['items'])
+
+    def choice_album(self):
+        albums = self._get_albums()
+        while True:
+            print('Введите номер альбома')
+            for index, elm in enumerate(albums):
+                print(f'{index}. {elm["title"]} ||| {elm["size"]} фотографии')
+            try:
+                choice_alb_num = int(input())
+            except ValueError:
+                print('Введите корректное число')
+                continue
+            if choice_alb_num in range(0, len(albums)):
+                id_album = albums[choice_alb_num]['id']
+                if id_album != -9000:
+                    self._get_photos_list(id_album)
+                    print('Success')
+                    break
+                else:
+                    self._get_user_photos_list()
+                    print('Success')
+                    break
+            else:
+                print('Введите корректное число')
+                continue
